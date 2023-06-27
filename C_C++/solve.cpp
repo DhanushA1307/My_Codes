1bb414f6ca53cd86ae9a48169957b861a29c434d
DhanushA1307 <dhanushanand1307@gmail.com>
2023-06-27 11:52:19 +0530

    changes in the code of Cplusplus

--- C_C++/solve.cpp
+++ C_C++/solve.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
-//using namespace std;
+using namespace std;
 
-char check(std::string &s)
+char check(string &s)
 {
-    std::map<char, int> mp;
+    map<char, int> mp;
     for(int i = 0; i < s.length(); i++) mp[s[i]] = int(s[i]);
-    std::vector<pair<char, int>> pr;
+    vector<pair<char, int>> pr;
     for(auto i : mp) pr.emplace_back(i.first,i.second);
     return pr[0].first;
 }
@@ -14,12 +14,12 @@ char check(std::string &s)
 int main(int argc, char** argv)
 {
     int N;
-    std::cin >> N;
+    cin >> N;
     for(int i = 1; i <= N; i++)
     {
-        std::string s;
-        std::cin >> s;
+        string s;
+        cin >> s;
         char c = check(s);
-        std::cout << c << " ";
+        cout << c << " ";
     }
 }
