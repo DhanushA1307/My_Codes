6299e16a8268445525c166ab4b59918f278d7889
DhanushA1307 <dhanushanand1307@gmail.com>
2023-07-05 01:18:03 +0530

    invalid phone number in cpp file

---
+++ C_C++/invalid_phone_number.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool valid(string &str)
+{
+    int c = 0;
+    map<char, int> mp;
+    for(auto i : str) 
+    {
+        mp[i]++;
+    }
+    for(auto i : mp)
+    {
+        if(i.first >= '0' && i.second <= '9')
+        {
+            c++;
+        }
+    }
+    if(c == 10 && str.length() == 10)
+    {
+        return true;
+    }
+    return false;
+}
+
+int main(int argc, char** argv)
+{
+    int count = 0;
+    string str, s2 = "q", s1 = "Q";
+    while(cin >> str)
+    {
+        if(s1 == str || s2 == str)
+        {
+            break;
+        }
+        else
+        {
+            if(!valid(str))
+            {
+                count++;
+            }
+        }
+    } 
+    cout << count;
+}
