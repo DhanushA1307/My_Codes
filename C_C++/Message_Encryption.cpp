e64540500fa663fd731e0157ff062c8a2bd880ca
DhanushA1307 <dhanushanand1307@gmail.com>
2023-07-09 00:09:07 +0530

    Successfully Message is Encryption

--- C_C++/Message_Encryption.cpp
+++ C_C++/Message_Encryption.cpp
@@ -3,9 +3,22 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    string s;
+    string s, f = "";
     int N;
     cin >> s >> N;
     vector<string> arr;
-    
+    for(int i = 0; i < s.length(); i+=N)
+    {
+        string a = s.substr(i,N);
+        if(arr.size() % 2 == 1) reverse(a.begin(), a.end());
+        arr.push_back(a);
+    }
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < arr.size(); j++)
+        {
+            f += arr[j][i];
+        }
+    }
+    cout << f;
 }
