f810f47f8dd915266d8dcf45945ac6bf2454b42b
DhanushA1307 <dhanushanand1307@gmail.com>
2023-07-10 00:16:22 +0530

    Modules in python with an examples

---
+++ python/example_modules/functions.py
@@ -0,0 +1,8 @@
+def square(x) -> int:
+    return (x*x)
+
+def cube(x) -> int:
+    return (x*x*x)
+
+def pow_of_2(x) -> int:
+    return(2**x)
---
+++ python/example_modules/sample.py
@@ -0,0 +1,6 @@
+import functions as f
+
+l = int(input())
+for i in range(l):
+    a = int(input())
+    print(f.pow_of_2(a),end = " ")
