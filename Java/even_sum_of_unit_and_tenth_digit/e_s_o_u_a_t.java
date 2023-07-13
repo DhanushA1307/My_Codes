08a27957d879813b31bb547b664aec31df3aedcb
DhanushA1307 <dhanushanand1307@gmail.com>
2023-07-13 10:05:32 -0900

    Little Changes

--- Java/even_sum_of_unit_and_tenth_digit/e_s_o_u_a_t.java
+++ Java/even_sum_of_unit_and_tenth_digit/e_s_o_u_a_t.java
@@ -5,17 +5,19 @@ public class e_s_o_u_a_t
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int n1 = sc.nextInt();
-        while(n <= n1)
+        try(Scanner sc = new Scanner(System.in))
         {
-            int unit = n % 10;
-            int tenth = (n / 10) % 10;
-        
-            if ((unit + tenth) % 2 == 0)
-                System.out.println(n);
-            n++;
+            int n = sc.nextInt();
+            int n1 = sc.nextInt();
+            while(n <= n1)
+            {
+                int unit = n % 10;
+                int tenth = (n / 10) % 10;
+            
+                if ((unit + tenth) % 2 == 0)
+                    System.out.println(n);
+                n++;
+            }
         }
     }
 }
