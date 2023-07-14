3257fd23a45db9ac99ea71081cccc4360913c593
Dhanush A <dhanushanand1307@gmail.com>
2023-07-14 21:38:40 +0530

    Little Changes
    Progression in the code statements

--- C_C++/Message_Encryption.cpp
+++ C_C++/Message_Encryption.cpp
@@ -3,22 +3,30 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    string s, f = "";
+    string s;
     int N;
+    
     cin >> s >> N;
-    vector<string> arr;
-    for(int i = 0; i < s.length(); i+=N)
+
+    vector<string> str_arr;
+    vector<char> str;
+
+    for(int i = 0; i < s.length(); i += N) str_arr.push_back(s.substr(i,N));
+    
+    for(int i = 0; i < N; i++) if(i % 2 == 1) reverse(str_arr[i].begin(), str_arr[i].end());
+
+    for(int i = 0, j = 0; i < N; i++) 
     {
-        string a = s.substr(i,N);
-        if(arr.size() % 2 == 1) reverse(a.begin(), a.end());
-        arr.push_back(a);
-    }
-    for(int i = 0; i < N; i++)
-    {
-        for(int j = 0; j < arr.size(); j++)
+        if((j+1) == N)
         {
-            f += arr[j][i];
+            break;
         }
+        if((i+1) == N)
+        {
+            j++;
+            i = 0;
+        }
+        str.push_back(str_arr[j][i]);
     }
-    cout << f;
+    for(auto i : str) cout << i << " ";
 }
