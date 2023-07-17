bd07ce49342782d88932d1b8b42f9f2e58d87952
DhanushA1307 <dhanushanand1307@gmail.com>
2023-07-17 22:52:42 +0530

    Invalid Phone Number

---
+++ Java/invalid_phone_number/invalidphonenumber.java
@@ -0,0 +1,32 @@
+package Java.invalid_phone_number;
+
+import java.util.*;
+
+public class invalidphonenumber
+{
+    public static void main(String[] args)
+    {
+        try(Scanner sc = new Scanner(System.in))
+        {
+            String s;
+            ArrayList<String> vec = new ArrayList<>();
+            while(true)
+            {
+                s = sc.nextLine();
+                if(s.equals("q"))
+                    break;
+                vec.add(s);
+            }
+            int valid = 0;
+            for(String i : vec)
+            {
+                if(i.matches("[0-9]{10}"))
+                {
+                    //System.out.println(i);
+                        valid++;
+                }
+            }
+            System.out.println(vec.size()-valid);
+        }
+    }
+}
