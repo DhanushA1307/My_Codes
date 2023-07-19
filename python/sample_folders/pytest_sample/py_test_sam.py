558bd4f0bf48c891cc4d4ac25c3324eac0437e84
DhanushA1307 <dhanushanand1307@gmail.com>
2023-07-19 13:11:07 +0530

    Py_test Sample

---
+++ python/sample_folders/pytest_sample/py_test_sam.py
@@ -0,0 +1,50 @@
+def add(a,b):
+    return (a+b)
+
+def mul(a,b):
+    return (a*b)
+
+
+def test_add():
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,5) == -25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(4,-5) == -1
+    assert mul(-5,-5) == 25
+    assert add(1,2) == 3
+    assert add(-4,-5) == (-10+1)
+    assert mul(-5,-5) == 25
