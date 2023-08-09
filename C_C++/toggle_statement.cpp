5deaabf1f75da0fecd6dfa9e0172ceed6754ff39
Dhanush A <dhanushanand1307@gmail.com>
2023-08-10 00:00:48 +0530

    Toggle Statement

---
+++ C_C++/toggle_statement.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.length(); i++)
+    {
+        cout << tolower(s[i]) << " ";
+    }
+}
