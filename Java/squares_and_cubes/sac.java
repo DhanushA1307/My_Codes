44c8d3ce8c8a13eefd517686d41a52eafe2b527c
Dhanush A <dhanushanand1307@gmail.com>
2023-08-30 21:46:49 +0530

    Squares and cubes of condition

---
+++ Java/squares_and_cubes/sac.java
@@ -0,0 +1,19 @@
+package My_Codes.Java.squares_and_cubes;
+
+import java.util.*;
+import java.io.*;
+
+public class sac
+{
+    public static void main(String[] args)
+    {
+        try(Scanner sc = new Scanner(System.in))
+        {
+            Long a = sc.nextLong(), b = sc.nextLong(), c = sc.nextLong();
+            if(a == b && a == c && b == c) System.out.print(Math.pow(a,3));
+            else if(a == b) System.out.print(Math.pow(c,2));
+            else if(a == c) System.out.print(Math.pow(b,2));
+            else if(b == c) System.out.print(Math.pow(a,2));
+        }
+    }
+}
