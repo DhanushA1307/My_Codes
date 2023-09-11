4bc901f8a9c68aad8129f5900fabd11a6dae6ad2
Dhanush A <dhanushanand1307@gmail.com>
2023-09-11 20:24:41 +0530

    Samples

---
+++ Java/samples/Samples.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Samples {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("You entered: " + num);
+        sc.close();
+    }
+}
