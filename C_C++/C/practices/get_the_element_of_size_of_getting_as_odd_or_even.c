45859261ac88ee61d3410749f9811e0d5b6d2e99
Dhanush A <dhanushanand1307@gmail.com>
2023-12-14 15:26:10 +0530

    Get the Element of size of getting the value as odd or even in depend on the size using pointers

---
+++ C_C++/C/practices/get_the_element_of_size_of_getting_as_odd_or_even.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<alloc.h>
+
+int* func(int N){
+    int* arr = malloc(sizeof(int)*N);
+    int j = ((N%2) ? 1 : 2);
+    for(int i = 0; i < N; i++){
+        arr[i] = (j += 2);
+    }
+    return arr;
+}
+
+int main(int argc, char** argv){
+    int N;
+    scanf("%d",&N);
+    int *arr = func(N);
+    for(int i = 0; i < N; i++) printf("%d ",*(arr+i));
+}
