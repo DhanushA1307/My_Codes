a7151a3b47e6ff73dfa5007b2a963ed205dbbe2f
Dhanush A <uit20109@rmd.ac.in>
2024-01-07 03:16:32 -0200

    Conversion of Two Dimensional array into the store in one dimensional array

---
+++ C_C++/CPP/practices/patterns/conversion_of _two_dimensional_array_into_one_dimensional_arry.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char** argv){
+	int N;
+	scanf("%d",&N);
+	int mat[N][N]; // 2 - Dimesional Array
+    
+    int rows[N*N]; // 1 - Dimesional Array
+	
+    for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			scanf("%d",&mat[i][j]);
+			rows[(i*N)+j] = mat[i][j];
+		}
+	}
+	for(int i = 0; i < (N*N); i++) printf("%d ", rows[i]);
+	printf("\n");
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++) printf("%d ", rows[(i*N)+j]);
+		printf("\n");
+	}
+}
